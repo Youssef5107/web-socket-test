08daad52e16c14a86d313f74aaf795ef3079b465
Youssef5107 <ybelalmarzouk@gmail.com>
2026-08-22 20:16:16 +0300

    feat:set server side logic

--- server/server.js
+++ server/server.js
@@ -7,25 +7,36 @@ const { Server } = require("socket.io");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://192.168.1.3:5173"],
   }),
 );
 
-// 1. Wrap Express in Node's HTTP server
 const server = http.createServer(app);
 
-// 2. Attach Socket.io to the HTTP server
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://192.168.1.3:5173"],
   },
 });
 
-app.get("/api/test", (req, res) => {
-  res.status(200).json({ message: "Success!" });
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User with ID: ${socket.id} joined room: ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
